e638d9837a5689479b9ea2e7eb97beae7b50ce77
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-02-07 13:43:36 +0100

    Added definitions of DDD building blocks to metamodel annotations.
    The definitions are heavily inspired by Eric Evans' DDD Reference [0].
    
    [0] https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf

--- src/main/java/foobar/core/annotation/Repository.java
+++ src/main/java/foobar/core/annotation/Repository.java
@@ -22,13 +22,21 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Identifies a {@link Repository}.
+ * Identifies a {@link Repository}. Repositories simulate a collection of aggregates to which aggregate instances can be
+ * added and removed. They usually also expose API to select a subset of aggregates matching certain criteria. Access to
+ * projections of an aggregate might be provided as well but also via a dedicated separate abstraction.
+ * <p>
+ * Implementations use a dedicated persistence mechanism appropriate to the data structure and query requirements at
+ * hand. However, they should make sure that no persistence mechanism specific APIs leak into client code.
  *
  * @author Christian Stettler
  * @author Henning Schwendtner
  * @author Stephan Pirnbaum
  * @author Martin Schimak
  * @author Oliver Drotbohm
+ * @see AggregateRoot
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Repositories</a>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
