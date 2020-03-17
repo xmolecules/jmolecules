7df0f0ed4cc645e9fc835a99e944bdbf90bc2045
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-03-17 16:39:42 +0100

    GH-2 - Added jddd-events module.
    Added @DomainEvent and interface alternative.
    
    Fixes #2.

---
+++ jddd-events/src/main/java/org/jddd/event/types/DomainEvent.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jddd.event.types;
+
+/**
+ * /** A domain event is a full-fledged part of the domain model, a representation of something that happened in the
+ * domain. It allows making the events that the domain experts want to track or be notified of explicit, or which are
+ * associated with state change in the other model objects.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwendtner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Domain Events</a>
+ */
+public interface DomainEvent {}
