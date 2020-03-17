cc5053731d96c5fd70b4719148c4151b2037b123
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-03-17 16:12:32 +0100

    GH-7 - Add type based alternatives for building blocks.
    Fixes #7.

---
+++ jddd-core/src/main/java/org/jddd/core/types/Entity.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jddd.core.types;
+
+/**
+ * Identifies an {@link Entity}. Entities represent a thread of continuity and identity, going through a lifecycle,
+ * though their attributes may change. Means of identification may come from the outside, or it may be an arbitrary
+ * identifier created by and for the system, but it must correspond to the identity distinctions in the model. The model
+ * must define what it means to be the same thing.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwendtner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Entities</a>
+ */
+public interface Entity<T extends AggregateRoot<T, ?>, ID> extends Identifiable<ID> {}
