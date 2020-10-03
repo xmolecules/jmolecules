ce879dff40073350c7c478c29d876794ed8fdb7b
Henning Schwentner <hs@wps.de>
2020-10-03 09:56:03 +0200

    GH-20 - Added @BoundedContext and @Module annotation.
    Added annotations for Bounded Context and Module.

---
+++ jmolecules-ddd/src/main/java/org/jmolecules/ddd/annotation/Module.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jddd.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Identifies a DDD module.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwentner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Bounded Contexts</a>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
+//TODO: with Java 9 or higher @Target(ElementType.MODULE)
+@Documented
+public @interface Module {
+
+}
