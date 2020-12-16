1d6b8d87187093ab24314a3cf040bbd588b99f3c
stephan.pirnbaum <stephan.pirnbaum@googlemail.com>
2020-12-16 18:50:51 +0100

    #18 added event sourcing annotations and publishers

---
+++ jmolecules-events/src/main/java/org/jmolecules/event/annotation/DomainEventPublisher.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmolecules.event.annotation;
+
+/**
+ * Identifies a domain event publisher, i.e. logic to publish a {@link DomainEvent}.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwentner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ */
+public @interface DomainEventPublisher {
+
+    /**
+     * Optional identification of the domain event published by this publisher.
+     * This information may be used for easier linkage between event and publisher
+     * by external tools and refers to the type of the domain event.
+     */
+    String publishes() default "";
+
+}
