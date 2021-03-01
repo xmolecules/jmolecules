c37a57728d979712fdea77c4d3f893a3d7ddb5a5
Oliver Drotbohm <odrotbohm@vmware.com>
2021-03-01 22:59:46 +0100

    GH-49 - Add interface based variant of @Repository.

---
+++ jmolecules-ddd/src/main/java/org/jmolecules/ddd/types/Repository.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmolecules.ddd.types;
+
+/**
+ * Identifies a {@link Repository}. Repositories simulate a collection of aggregates to which aggregate instances can be
+ * added and removed. They usually also expose API to select a subset of aggregates matching certain criteria. Access to
+ * projections of an aggregate might be provided as well but also via a dedicated separate abstraction.
+ * <p>
+ * Implementations use a dedicated persistence mechanism appropriate to the data structure and query requirements at
+ * hand. However, they should make sure that no persistence mechanism specific APIs leak into client code.
+ *
+ * @author Christian Stettler
+ * @author Henning Schwentner
+ * @author Stephan Pirnbaum
+ * @author Martin Schimak
+ * @author Oliver Drotbohm
+ * @see AggregateRoot
+ * @see <a href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+ *      Reference (Evans) - Repositories</a>
+ * @since 1.2
+ */
+public interface Repository<T extends AggregateRoot<T, ID>, ID extends Identifier> {}
