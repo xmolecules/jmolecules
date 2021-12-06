10eaeff0b88a185f4c00bb39b375d625208ef7c7
Mahatma_Fatal_Error <MahatmaFatalError@users.noreply.github.com>
2021-12-06 21:58:17 +0100

    adds annotations for hexagonal architecture

---
+++ jmolecules-architecture/jmolecules-onion-architecture/src/main/java/org/jmolecules/architecture/onion/hexagonal/OutputPort.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmolecules.architecture.onion.hexagonal;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Identifies the {@link OutputPort} in an hexagonal architecture. 
+ * An outbound port define the core’s view of the outside world. This are the interface the core need to communicate with the outside world.
+ *
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.PACKAGE, ElementType.TYPE })
+@Documented
+public @interface OutputPort {
+}
